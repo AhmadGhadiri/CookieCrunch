412f54249f4f03039ebe6bc59b15f78da0706d7e
Ahmad Ghadiri <ahmad@Ahmads-MacBook-Pro.local>
2015-04-08 14:09:48 +0300

    first commit

---
+++ CookieCrunch/Cookie.swift
@@ -0,0 +1,26 @@
+//
+//  Cookie.swift
+//  CookieCrunch
+//
+//  Created by Ahmad Ghadiri on 4/8/15.
+//  Copyright (c) 2015 Ahmad Ghadiri. All rights reserved.
+//
+
+import SpriteKit
+
+enum CookieType: Int {
+    case Unknown = 0, Croissant, Cupcake, Danish, Donut, Macaroon, SugarCookie
+}
+
+class Cookie {
+    var column: Int
+    var row: Int
+    let cookieType: CookieType
+    var sprite: SKSpriteNode?
+    
+    init(column: Int, row: Int, cookieType: CookieType) {
+        self.column = column
+        self.row = row
+        self.cookieType = cookieType
+    }
+}
