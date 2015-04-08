9ef1e399a868ddab164ef19c40f11419013f781d
Ahmad <a_ghadiri1365@yahoo.com>
2015-04-08 14:26:04 +0300

    Until Get Ready, Get Set

---
+++ CookieCrunch/Array2D.swift
@@ -0,0 +1,28 @@
+//
+//  Array2D.swift
+//  CookieCrunch
+//
+//  Created by Ahmad Ghadiri on 4/8/15.
+//  Copyright (c) 2015 Ahmad Ghadiri. All rights reserved.
+//
+
+struct Array2D<T> {
+    let columns: Int
+    let rows: Int
+    private var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        array = Array<T?>(count: rows*columns, repeatedValue: nil)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[row*columns + column]
+        }
+        set {
+            array[row*columns + column] = newValue
+        }
+    }
+}
