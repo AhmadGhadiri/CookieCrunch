b214c6976e5f38f13058a4b48d888807e819c1e2
Ahmad Ghadiri <ahmad@Ahmads-MacBook-Pro.local>
2015-04-08 14:18:25 +0300

    Up to Keeping the Cookies: the 2-D Grid

--- CookieCrunch/Cookie.swift
+++ CookieCrunch/Cookie.swift
@@ -8,15 +8,46 @@
 
 import SpriteKit
 
-enum CookieType: Int {
+enum CookieType: Int, Printable {
     case Unknown = 0, Croissant, Cupcake, Danish, Donut, Macaroon, SugarCookie
+    
+    
+    var spriteName: String {
+        let spriteNames = [
+            "Croissant",
+            "Cupcake",
+            "Danish",
+            "Donut",
+            "Macaroon",
+            "SugarCookie"]
+        
+        return spriteNames[rawValue - 1]
+    }
+    
+    var highlightedSpriteName: String {
+        return spriteName + "-Highlighted"
+    }
+    
+    static func random() -> CookieType {
+        return CookieType(rawValue: Int(arc4random_uniform(6)) + 1)!
+    }
+    
+    // For Printable
+    var description: String {
+        return spriteName
+    }
 }
 
-class Cookie {
+class Cookie: Printable {
     var column: Int
     var row: Int
     let cookieType: CookieType
     var sprite: SKSpriteNode?
+    
+    //for printable
+    var description: String {
+        return "type:\(cookieType) square:(\(column),\(row))"
+    }
     
     init(column: Int, row: Int, cookieType: CookieType) {
         self.column = column
